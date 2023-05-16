ea49ec46a0b1f4930c3c0654ccede1ae12583cc0
Hacci Morihata <morihata@nhkcma.com>
2023-05-16 14:09:15 -0400

    Add project files.

---
+++ mhacci_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("mhacci_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("mhacci_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("B38C4C1E-7388-45F9-8272-16D52B5821FF")]
+[assembly: AssemblyVersion("1.0.0.0")]
